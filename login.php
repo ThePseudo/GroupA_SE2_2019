75af27d9f4c91e6b9846b43afa7b04a90df5cf49
Andrea Calabrese <s265340@studenti.polito.it>
2019-10-15 17:41:51 +0200

    Unique login (?), home button added

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+require_once("base/templates.php");
+top("Office queues - login");
+?>
+<h1>Log in employee</h1>
+<br /><br /><br /><br /><br /><br />
+<a href="index.php" style="color:black;text-decoration:none;"><button class="topright loginbtn">Home</button></a>
+<div style="display:block; text-align:center; align-content:center; margin:auto">
+    <form action="actions/log_in.php" method="get">
+
+        <a style="font-size:20px;">Username</a><input type="text" name="id" class="text" /> <br />
+        <a style="font-size:20px;">Password</a><input type="password" name="pwd" class="text" />
+
+        <br />
+        <input type="submit" class="loginbtn" style="margin-top:40px;" value="Sign in">
+
+    </form>
+</div>
+<?php
+bottom();
