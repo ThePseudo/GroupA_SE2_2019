f398b437bd0091052cf73afd9be89e93092ff778
Andrea Calabrese <s265340@studenti.polito.it>
2019-10-15 17:01:05 +0200

    Created main page + css

--- index.php
+++ index.php
@@ -5,8 +5,12 @@ top("Office queues - main");
 <h1>Welcome!</h1>
 <h1>Which service do you want to select?</h1>
 
-<button class="topright loginbtn">Login</button>
+<a href="login.php" style="color:black;text-decoration:none;"><button class="topright loginbtn">Login</button></a>
 
+<div style="display:block; text-align:center; align-content:center; margin:auto;">
+    <button class="selectbtn">A - ACCOUNTING</button></br>
+    <button class="selectbtn">P - PACKAGES</button>
+</div>
 <?php
 bottom();
 ?>
