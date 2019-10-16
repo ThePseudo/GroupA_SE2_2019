b94bd9fa8fd6dca998a8202e6c3a9f72c93080f6
Andrea Calabrese <s265340@studenti.polito.it>
2019-10-16 09:00:18 +0200

    Added checks to $_SESSION['id']

--- actions/next.php
+++ actions/next.php
@@ -3,5 +3,7 @@
 // session[id] = counter
 require_once("functions.php");
 session_start();
-serveNext($_SESSION['id']);
+if (isset($_SESSION['id'])) {
+    serveNext($_SESSION['id']);
+}
 header("Location: ../operator_home.php");
--- operator_home.php
+++ operator_home.php
@@ -2,15 +2,17 @@
 require_once("base/templates.php");
 session_start();
 top("Office queues - operator");
-?>
+if (isset($_SESSION['id'])) {
+    ?>
 
-<h1>Welcome, operator <?php echo $_SESSION['id']; ?></h1>
-<br /><br /><br />
-<div style="display:block; text-align:center; align-content:center; margin:auto;">
-    <form action="actions/next.php" method="POST">
-        <input type="submit" class="selectbtn" style="margin-top:40px;" value="Next user">
-    </form>
-</div>
+    <h1>Welcome, operator <?php echo $_SESSION['id']; ?></h1>
+    <br /><br /><br />
+    <div style="display:block; text-align:center; align-content:center; margin:auto;">
+        <form action="actions/next.php" method="POST">
+            <input type="submit" class="selectbtn" style="margin-top:40px;" value="Next user">
+        </form>
+    </div>
 <?php
+}
 bottom();
 ?>
