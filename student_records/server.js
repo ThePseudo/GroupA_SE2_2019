3fbedf3d0b17a66b675ae145a8b51a36599acefb
Andrea Calabrese <s265340@studenti.polito.it>
2019-10-30 12:47:44 +0100

    Renewed Node.js

--- student_records/server.js
+++ student_records/server.js
@@ -1,16 +1,26 @@
 'use strict';
 
 const express = require('express');
+const http = require('http');
 
 // Constants
 const PORT = 8000;
 const HOST = '0.0.0.0';
 
 // App
+/*
 const app = express();
 app.get('/', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.send('Hello world\n');
 });
 
 app.listen(PORT, HOST);
+*/
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, world!');
+}).listen(PORT);
+
 console.log(`Running on http://${HOST}:${PORT}`);
