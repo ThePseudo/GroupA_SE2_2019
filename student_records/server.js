8e6c829f795c2886195dc9d16ec6aaee90c5684e
Andrea Calabrese <s265340@studenti.polito.it>
2019-11-06 17:14:42 +0100

    First tries with Node.js + base page setup base

--- student_records/server.js
+++ student_records/server.js
@@ -1,26 +1,27 @@
 'use strict';
 
 const express = require('express');
-const http = require('http');
+const fs = require('fs');
 
 // Constants
 const PORT = 8000;
 const HOST = '0.0.0.0';
 
 // App
-/*
 const app = express();
 app.get('/', (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.send('Hello world\n');
+    var page = '';
+    fs.readFile("pages/base/base.html", function (err, data) {
+        page = data;
+
+        // HTML here!
+
+        fs.readFile("pages/base/end.html", function (err, data) {
+            page += data;
+            res.end(page);
+        });
+    });
 });
 
 app.listen(PORT, HOST);
-*/
-
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/HTML' });
-    res.end('<!DOCTYPE html>\n<h1>Hello, world!</h1>');
-}).listen(PORT);
-
 console.log(`Running on http://${HOST}:${PORT}`);
