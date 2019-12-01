4f682961c30455c34e6c70f04fa197008d27772c
Andrea Calabrese <s265340@studenti.polito.it>
2019-12-01 09:52:09 +0100

    Fixed startup and parent course homepage view

--- student_records/startup.sh
+++ student_records/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dpkg-reconfigure mysql-server-5.7
 service mysql start
-mkdir ./uploads/material
+mkdir -p ./uploads/materials
 mysql -u root "-ppwd" -e "Alter USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'pwd'"
 mysql -u root "-ppwd" -e "CREATE DATABASE students; USE students;"
 mysql -u root "-ppwd" students -e "source ./db/init_db.sql"
