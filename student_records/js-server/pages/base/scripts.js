afa1db35aee28c391859e9af56fecc959d8b04ed
Andrea Calabrese <s265340@studenti.polito.it>
2019-12-04 15:53:39 +0100

    Improved parent homepage + navbar, solved issue where navbar was not shown in small parent homepage and did not allow logout

--- student_records/js-server/pages/base/scripts.js
+++ student_records/js-server/pages/base/scripts.js
@@ -2,10 +2,12 @@ var sidebarVisible = false;
 function onMenuButtonClick() {
     if (sidebarVisible) {
         document.getElementById("sidebar").style.marginLeft = "-100%";
+        document.getElementById("navtoggle").classList.remove("activeMenuButton");
         sidebarVisible = false;
     }
     else {
         document.getElementById("sidebar").style.marginLeft = "0px";
+        document.getElementById("navtoggle").classList.add("activeMenuButton");
         sidebarVisible = true;
     }
 }
