2bd080268af79beaefe7eac295e4e9d770732889
Savik94 <savik94sd@gmail.com>
2019-12-10 10:12:01 +0100

    Added principal register, login and some changes to db

--- student_records/js-server/modules/admin.js
+++ student_records/js-server/modules/admin.js
@@ -76,7 +76,7 @@ router.post("/reg_officer", (req, res) => {
             res.end("Count impossible to compute");
             return;
         }
-        con.query("INSERT INTO officer(id, first_name, last_name, cod_fisc, email, password, first_access) VALUES(?, ?, ?, ?, ?, ?, ?)", [rows[0].c + 1, name, surname, SSN, email, hash_pwd, 0], (err, result) => {
+        con.query("INSERT INTO officer(id, first_name, last_name, cod_fisc, email, password, first_access,principal) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", [rows[0].c + 1, name, surname, SSN, email, hash_pwd, 0, 0], (err, result) => {
             if (err) {
                 res.end("There is a problem in the DB connection. Please, try again later " + err);
                 return;
@@ -91,7 +91,7 @@ router.post("/reg_officer", (req, res) => {
 
 //COMMENTO MOMENTANEAMENTE ROUTE PER ENROLL PRINCIPAL
 
-/* router.post("/reg_principal", (req, res) => {
+router.post("/reg_principal", (req, res) => {
     let name = req.body.name;
     let surname = req.body.surname;
     let SSN = req.body.SSN;
@@ -107,7 +107,7 @@ router.post("/reg_officer", (req, res) => {
         insecureAuth: true
     });
 
-    con.query('SELECT COUNT(*) as c FROM collaborator', (err, rows, fields) => { // because we have no AUTO_UPDATE available on the DB
+    con.query('SELECT COUNT(*) as c FROM officer', (err, rows, fields) => { // because we have no AUTO_UPDATE available on the DB
         if (err) {
             res.end("There is a problem in the DB connection. Please, try again later " + err);
             return;
@@ -116,7 +116,7 @@ router.post("/reg_officer", (req, res) => {
             res.end("Count impossible to compute");
             return;
         }
-        con.query("INSERT INTO collaborator(id, first_name, last_name, cod_fisc, email, password, first_access) VALUES(?, ?, ?, ?, ?, ?, ?)", [rows[0].c + 1, name, surname, SSN, email, hash_pwd, 1], (err, result) => {
+        con.query("INSERT INTO officer (id, first_name, last_name, cod_fisc, email, password, first_access, principal) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", [rows[0].c + 1, name, surname, SSN, email, hash_pwd, 0, 1], (err, result) => {
             if (err) {
                 res.end("There is a problem in the DB connection. Please, try again later " + err);
                 return;
@@ -127,7 +127,7 @@ router.post("/reg_officer", (req, res) => {
             res.redirect("/admin/enroll_principal");
         });
     });
-}); */
+});
 
 router.post("/reg_teacher", (req, res) => {
     let name = req.body.name;
