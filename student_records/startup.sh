0fd749f9338041db63565813aaa22fb42a95b598
Andrea Calabrese <s265340@studenti.polito.it>
2019-12-26 09:26:29 +0100

    Improved startup (showing up less useless things)

--- student_records/startup.sh
+++ student_records/startup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-dpkg-reconfigure mysql-server-5.7
-service mysql start
+echo "Starting MySql"
+dpkg-reconfigure mysql-server-5.7 > /dev/null 2>&1
+service mysql start > /dev/null 2>&1
 mkdir -p ./uploads/materials
-mysql -u root "-ppwd" -e "Alter USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'pwd'"
-mysql -u root "-ppwd" -e "CREATE DATABASE students; USE students;"
+mysql -u root "-ppwd" -e "Alter USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'pwd'" > /dev/null 2>&1
+mysql -u root "-ppwd" -e "CREATE DATABASE students; USE students;" > /dev/null 2>&1
 mysql -u root "-ppwd" students -e "source ./db/init_db.sql"
 mysql -u root "-ppwd" students -e "source ./db/populate.sql"
+echo "MySql Ok"
 rm -rf ./db
 rm -f startup.sh
+echo "Starting Node"
 node server.js &
 /bin/bash
