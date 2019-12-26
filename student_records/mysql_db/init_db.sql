97315a3c23ef8d6f07847999cceb94571dc514dd
Andrea Calabrese <s265340@studenti.polito.it>
2019-12-26 09:08:11 +0100

    Separated creation and population of db + docker is interactive now

--- student_records/mysql_db/init_db.sql
+++ student_records/mysql_db/init_db.sql
@@ -13,16 +13,6 @@ CREATE TABLE teacher
     /* 1 first_access already done; 0 not yet */
 );
 
-INSERT INTO teacher
-    (id,first_name,last_name,cod_fisc,email,password,first_access)
-VALUES
-    (1, "Elena", "Baralis", "AV85T", "elena.baralis.polito@gmail.com", "$2a$10$0tXRERd11hkw3zKQQmFeTOAuUcMiI6/ThiMNvfMUvKmYkWkL0BRkO", 1);
-
-INSERT INTO teacher
-    (id,first_name,last_name,cod_fisc,email,password,first_access)
-VALUES
-    (2, "Marina", "Indri", "RVEQXX32E18B392G", "marina64@yahoo.com", "$2a$10$0tXRERd11hkw3zKQQmFeTOAuUcMiI6/ThiMNvfMUvKmYkWkL0BRkO", 1);
-
 CREATE TABLE officer
 (
     id INT PRIMARY KEY,
@@ -35,16 +25,6 @@ CREATE TABLE officer
     principal BOOLEAN NOT NULL
 );
 
-INSERT INTO officer
-    (id,first_name,last_name,cod_fisc,email,password,first_access, principal)
-VALUES
-    (1, "Giorno", "Giovanna", "CP80X", "pasticcio80@gmail.com", "$2a$10$0tXRERd11hkw3zKQQmFeTOAuUcMiI6/ThiMNvfMUvKmYkWkL0BRkO", 1, 0);
-
-INSERT INTO officer
-    (id,first_name,last_name,cod_fisc,email,password,first_access, principal)
-VALUES
-    (2, "Tommaso", "Bodda", "FTEZMF95C47E840N", "bodda75@gmail.com", "$2a$10$0tXRERd11hkw3zKQQmFeTOAuUcMiI6/ThiMNvfMUvKmYkWkL0BRkO", 1, 0);
-
 CREATE TABLE parent
 (
     id INT PRIMARY KEY,
@@ -55,20 +35,6 @@ CREATE TABLE parent
     password VARCHAR(255) NOT NULL,
     first_access BOOLEAN NOT NULL
 );
-
-INSERT INTO parent
-    (id, first_name, last_name,cod_fisc,email,password, first_access)
-VALUES
-    (1, "Alberto", "Rosso", "1111", "alberto.rosso@gmail.com", "$2a$10$0tXRERd11hkw3zKQQmFeTOAuUcMiI6/ThiMNvfMUvKmYkWkL0BRkO", 1);
-
-INSERT INTO parent
-    (id, first_name, last_name,cod_fisc,email,password, first_access)
-VALUES
-    (2, "Piero", "Fetta", "HDDRWU63L46D667M", "giorno@giovanna.com", "$2a$10$0tXRERd11hkw3zKQQmFeTOAuUcMiI6/ThiMNvfMUvKmYkWkL0BRkO", 1);
-INSERT INTO parent
-    (id, first_name, last_name,cod_fisc,email,password, first_access)
-VALUES
-    (3, "Jotaro", "Kujo", "DZJVSL56R55D763T", "marco@verdi.com", "$2a$10$0tXRERd11hkw3zKQQmFeTOAuUcMiI6/ThiMNvfMUvKmYkWkL0BRkO", 1);
 
 CREATE TABLE student
 (
@@ -81,52 +47,11 @@ CREATE TABLE student
     parent_2 VARCHAR(16)
 );
 
-INSERT INTO student
-    (id,first_name,last_name,cod_fisc,class_id,parent_1,parent_2)
-VALUES
-    (1, "Angelo", "Rosso", "NJRGTD35P14B145Z", 2, 1, 2);
-
-INSERT INTO student
-    (id,first_name,last_name,cod_fisc,class_id,parent_1,parent_2)
-VALUES
-    (2, "Martino", "Arte", "RLIZFH80S05E249F", 2, 1, 2);
-
-INSERT INTO student
-    (id,first_name,last_name,cod_fisc,class_id,parent_1,parent_2)
-VALUES
-    (3, "Martinella", "Leone", "JDKDMY59L04H816S", 2, 1, 2);
-
-INSERT INTO student
-    (id,first_name,last_name,cod_fisc,class_id,parent_1,parent_2)
-VALUES
-    (4, "Serena", "Fetta", "BSOWTD48P60F492T", 2, 2, 3);
-
-INSERT INTO student
-    (id,first_name,last_name,cod_fisc,class_id,parent_1,parent_2)
-VALUES
-    (5, "Francesca", "Fetta", "DVBGDD76L56E934Y", 2, 2, 3);
-
--- Classes
-
 CREATE TABLE class
 (
     id INT PRIMARY KEY,
     class_name VARCHAR(2) UNIQUE NOT NULL
 );
-
-INSERT INTO class
-    (id,class_name)
-VALUES(1, "1A");
-
-INSERT INTO class
-    (id,class_name)
-VALUES(2, "1B");
-
-INSERT INTO class
-    (id,class_name)
-VALUES(3, "1C");
-
--- courses
 
 CREATE TABLE course
 (
@@ -134,37 +59,6 @@ CREATE TABLE course
     course_name VARCHAR(50) UNIQUE NOT NULL,
     color VARCHAR(6) UNIQUE NOT NULL
 );
-
-INSERT INTO course
-    (id,course_name, color)
-VALUES
-    (1, 'Math', 'FF0000');
-
-INSERT INTO course
-    (id,course_name, color)
-VALUES
-    (2, 'History', '0000FF');
-
-INSERT INTO course
-    (id,course_name, color)
-VALUES
-    (3, 'Science', '00FF00');
-
-INSERT INTO course
-    (id,course_name, color)
-VALUES
-    (4, 'Chemistry', "008080");
-
-INSERT INTO course
-    (id,course_name, color)
-VALUES
-    (5, 'Art', '008000');
-
-INSERT INTO course
-    (id,course_name, color)
-VALUES
-    (6, 'Geography', '00FFFF');
-
 
 CREATE TABLE admin
 (
@@ -176,16 +70,6 @@ CREATE TABLE admin
     password VARCHAR(255) NOT NULL
 );
 
-INSERT INTO admin
-    (id,first_name,last_name,cod_fisc,email,password)
-VALUES
-    (1, "Giovanni", "Girrgio", "GPS67", "rossi@yahoo.com", "$2a$10$0tXRERd11hkw3zKQQmFeTOAuUcMiI6/ThiMNvfMUvKmYkWkL0BRkO");
-
-INSERT INTO admin
-    (id,first_name,last_name,cod_fisc,email,password)
-VALUES
-    (2, "Arturo", "Merzario", "SJINBL63M03B687S", "racers@yahoo.com", "$2a$10$0tXRERd11hkw3zKQQmFeTOAuUcMiI6/ThiMNvfMUvKmYkWkL0BRkO");
-
 CREATE TABLE topic
 (
     id INT UNIQUE NOT NULL,
@@ -195,19 +79,6 @@ CREATE TABLE topic
     description TEXT NOT NULL,
     PRIMARY KEY(id, topic_date, id_class, id_course)
 );
-
-INSERT INTO topic
-    (id, topic_date, id_class, id_course, description)
-VALUES(1, "2019-09-30", 1, 1, "Monoms");
-
-INSERT INTO topic
-    (id, topic_date, id_class, id_course, description)
-VALUES(2, "2019-09-29", 2, 1, "Monoms");
-
-INSERT INTO topic
-    (id, topic_date, id_class, id_course, description)
-VALUES(3, "2019-09-30", 2, 1, "Polynoms");
-
 
 CREATE TABLE note
 (
@@ -220,36 +91,6 @@ CREATE TABLE note
     PRIMARY KEY(id)
 );
 
-INSERT INTO note
-    (id, student_id, teacher_id, note_date, motivation, justified)
-VALUES(1, 1, 1, "2019-09-30", "L'alunno fa rumore in classe", 1);
-
-INSERT INTO note
-    (id, student_id, teacher_id, note_date, motivation, justified)
-VALUES(2, 1, 2, "2019-09-29", "L'alunno fa ANCORA rumore in classe", 0);
-
-INSERT INTO note
-    (id, student_id, teacher_id, note_date, motivation, justified)
-VALUES(3, 1, 1, "2019-09-29", "L'alunno fa ANCORA rumore 3 ora provo a mettere una stringa lunga, ancora più lunga vediamo ora", 0);
-
-INSERT INTO note
-    (id, student_id, teacher_id, note_date, motivation, justified)
-VALUES(4, 1, 2, "2019-09-29", "L'alunno fa ANCORA rumore 4", 0);
-
-INSERT INTO note
-    (id, student_id, teacher_id, note_date, motivation, justified)
-VALUES(5, 1, 2, "2019-09-29", "L'alunno fa ANCORA rumore 5", 1);
-
-
-INSERT INTO note
-    (id, student_id, teacher_id, note_date, motivation, justified)
-VALUES(6, 2, 1, "2019-09-29", "L'alunno fa ANCORA rumore 4", 0);
-
-INSERT INTO note
-    (id, student_id, teacher_id, note_date, motivation, justified)
-VALUES(7, 2, 2, "2019-09-29", "L'alunno fa ANCORA rumore 5", 1);
-
-
 CREATE TABLE absence
 (
     id INT ,
@@ -261,44 +102,6 @@ CREATE TABLE absence
     PRIMARY KEY(id)
 );
 
-INSERT INTO absence
-    (id, student_id, date_ab, start_h, end_h, justified)
-VALUES(1, 1, "2019-09-30", "08:00", "13:15", 1);
-
-INSERT INTO absence
-    (id, student_id, date_ab, start_h, end_h, justified)
-VALUES(2, 1, "2019-09-29", "08:00", "13:15", 0);
-
-INSERT INTO absence
-    (id, student_id, date_ab, start_h, end_h, justified)
-VALUES(3, 1, "2019-10-20", "08:00", "13:15", 1);
-
-INSERT INTO absence
-    (id, student_id, date_ab, start_h, end_h, justified)
-VALUES(4, 1, "2019-10-21", "08:00", "13:15", 1);
-
-INSERT INTO absence
-    (id, student_id, date_ab, start_h, end_h, justified)
-VALUES(5, 1, "2019-10-22", "08:00", "13:15", 1);
-
-INSERT INTO absence
-    (id, student_id, date_ab, start_h, end_h, justified)
-VALUES(6, 1, "2019-10-10", "08:00", "13:15", 0);
-
-INSERT INTO absence
-    (id, student_id, date_ab, start_h, end_h, justified)
-VALUES(7, 2, "2019-09-30", "08:00", "13:15", 1);
-
-INSERT INTO absence
-    (id, student_id, date_ab, start_h, end_h, justified)
-VALUES(8, 2, "2019-09-29", "08:00", "13:15", 0);
-
-INSERT INTO absence
-    (id, student_id, date_ab, start_h, end_h, justified)
-VALUES(9, 3, "2019-09-30", "08:00", "13:15", 1);
-
--- Homework
-
 CREATE TABLE homework
 (
     id INT UNIQUE NOT NULL,
@@ -308,24 +111,6 @@ CREATE TABLE homework
     date_hw DATE NOT NULL,
     PRIMARY KEY(id)
 );
-
-INSERT INTO homework
-    (id, course_id, class_id, description, date_hw)
-VALUES
-    (1, 1, 1, "Study pages 5-6-7", "2019-10-17");
-
--- Path: Angelo Rosso -> Science -> materials and homeworks
-INSERT INTO homework
-    (id, course_id, class_id, description, date_hw)
-VALUES
-    (2, 3, 2, "Study pages 5-6-7", "2019-10-17");
-
-
-INSERT INTO homework
-    (id, course_id, class_id, description, date_hw)
-VALUES
-    (3, 3, 2, "Ex 1 pag 12", "2019-10-17");
--- Materials
 
 CREATE TABLE material
 (
@@ -337,38 +122,6 @@ CREATE TABLE material
     date_mt DATE NOT NULL,
     PRIMARY KEY(id)
 );
-
-INSERT INTO material
-    (id,course_id, class_id, description, link, date_mt)
-VALUES
-    (1, 3, 2, "questo è un test per vedere come si comporta la tabella con stringhe lunghe", "test.txt", '2019-9-12');
-
-INSERT INTO material
-    (id,course_id, class_id, description, link, date_mt)
-VALUES
-    (2, 3, 2, "prova descrizione1", "file.pdf", '2019-9-12');
-
-INSERT INTO material
-    (id,course_id, class_id, description, link, date_mt)
-VALUES
-    (3, 3, 2, "prova descrizione2", "file.pdf", '2019-9-12');
-
-INSERT INTO material
-    (id,course_id, class_id, description, link, date_mt)
-VALUES
-    (4, 3, 2, "prova descrizione3", "file.pdf", '2019-9-12');
-
-INSERT INTO material
-    (id,course_id, class_id, description, link, date_mt)
-VALUES
-    (5, 3, 2, "prova descrizione4", "file.pdf", '2019-9-12');
-
-INSERT INTO material
-    (id,course_id, class_id, description, link, date_mt)
-VALUES
-    (6, 3, 2, "prova descrizione5", "file.pdf", '2019-9-12');
-
--- Marks
 
 CREATE TABLE mark
 (
@@ -383,23 +136,16 @@ CREATE TABLE mark
     type_mark_subj ENUM ('Other','Written','Oral','Project')
 );
 
-INSERT INTO mark
-    (id,student_id, course_id, score, date_mark, period_mark,mark_subj,descr_mark_subj,type_mark_subj)
-VALUES
-    (1, 1, 1, 6, '2019-9-10', 1, 'Chim 1', '3 domande', 'Other');
-
-INSERT INTO mark
-    (id,student_id, course_id, score, date_mark, period_mark,mark_subj,descr_mark_subj,type_mark_subj)
-VALUES
-    (2, 1, 2, 8, '2019-9-11', 2, 'Chim 1', '3 domande', 'Other');
-
-INSERT INTO mark
-    (id,student_id, course_id, score, date_mark, period_mark,mark_subj,descr_mark_subj,type_mark_subj)
-VALUES
-    (3, 1, 3, 10, '2019-9-12', 1, 'Chim 1', '3 domande', 'Other');
+CREATE TABLE General_Communication
+(
+    id INT PRIMARY KEY,
+    communication TEXT,
+    comm_date DATE
+);
 
 -- RELATIONS
 
+-- Maybe delete (?) Not sure. TODO: review with team
 CREATE TABLE student_class
 (
     student_id INT,
@@ -418,37 +164,3 @@ CREATE TABLE teacher_course_class
     year INT NOT NULL,
     PRIMARY KEY(teacher_id, course_id, class_id, year)
 );
-
-INSERT INTO teacher_course_class
-    (teacher_id, course_id, class_id, year)
-VALUES(1, 1, 1, 2019);
-
-INSERT INTO teacher_course_class
-    (teacher_id, course_id, class_id, year)
-VALUES(1, 3, 1, 2019);
-
-INSERT INTO teacher_course_class
-    (teacher_id, course_id, class_id, year)
-VALUES(1, 3, 2, 2019);
-
-CREATE TABLE General_Communication
-(
-    id INT PRIMARY KEY,
-    communication TEXT,
-    comm_date DATE
-);
-
-INSERT INTO General_Communication
-    (id,communication, comm_date)
-VALUES
-    (1, "2 million euros for a 5 years grant at Politecnico di Torino to pave new routes for the design of innovative materials with various technological applications. Giovanni Maria Pavan has been awarded a prestigious European Research Council (ERC) Consolidator Grant with his research project DYNAPOL - Modeling approaches toward bioinspired dynamic materials", '2019-11-26');
-
-INSERT INTO General_Communication
-    (id,communication, comm_date)
-VALUES
-    (2, "In order to raise awareness inside our University on the wide variation of this concept, adv. Arianna Enrichens, Politecnico Confidential Counsellor, prepared a short video to inform the community about verbal violence on the web as well as on social media.o", '2019-11-26');
-
-INSERT INTO General_Communication
-    (id,communication, comm_date)
-VALUES
-    (3, "See the pdf on site", '2019-11-27');
--- student_records/startup.sh
+++ student_records/startup.sh
@@ -5,4 +5,8 @@ mkdir -p ./uploads/materials
 mysql -u root "-ppwd" -e "Alter USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'pwd'"
 mysql -u root "-ppwd" -e "CREATE DATABASE students; USE students;"
 mysql -u root "-ppwd" students -e "source ./db/init_db.sql"
-node server.js
+mysql -u root "-ppwd" students -e "source ./db/populate.sql"
+rm -rf ./db
+rm -f startup.sh
+node server.js &
+/bin/bash
